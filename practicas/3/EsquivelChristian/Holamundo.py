06ecbda9f2b6497c03bfa02c1e783dfeb71af060
chrxz12 <chris.esq.san@gmail.com>
2024-08-20 20:52:51 -0600

    Se realizaron modificaciones en el código

--- practicas/3/EsquivelChristian/Holamundo.py
+++ practicas/3/EsquivelChristian/Holamundo.py
@@ -1,1 +1,7 @@
-print ("Hola mundo")
+import getpass
+
+# Obtener el nombre del usuario del sistema
+nombre_usuario = getpass.getuser()
+
+# Imprimir el saludo personalizado
+print(f"Hola, {nombre_usuario}")
