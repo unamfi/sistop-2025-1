3a7727057a50b0d8018a77dd4269333fa9273387
Enrique Medrano Solano <enriquemedranorabak@hotmail.com>
2024-08-24 23:21:14 -0600

    Se modifica holaMundo.py solo para mejorar errores ortograficos y visuales

--- practicas/3/MedranoEnrique/holaMundo.py
+++ practicas/3/MedranoEnrique/holaMundo.py
@@ -1,4 +1,4 @@
 """programa para la práctica 3 S.O"""
 
-nombre = input("¿Cual es tu nombre?")
-print(f"¿Hola Mundo o Hola {nombre}\n\n ¡No importa, solo se feliz como una lombriz!!")
+nombre = input("¿Cual es tu nombre?\n")
+print(f"\n¿Hola Mundo o Hola {nombre}?\n\n¡No importa, solo se feliz como una lombriz!!")
