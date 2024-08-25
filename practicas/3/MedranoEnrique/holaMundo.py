80b03b7418b6ddc1f1ff8067157dbe91b03e607a
Enrique Medrano Solano <enriquemedranorabak@hotmail.com>
2024-08-24 23:16:48 -0600

    Se modifica holaMundo.py para el saludo personal, se pide nombre desde línea de comandos

--- practicas/3/MedranoEnrique/holaMundo.py
+++ practicas/3/MedranoEnrique/holaMundo.py
@@ -1,3 +1,4 @@
 """programa para la práctica 3 S.O"""
 
-print("¡Hola Mundo!")
+nombre = input("¿Cual es tu nombre?")
+print(f"¿Hola Mundo o Hola {nombre}\n\n ¡No importa, solo se feliz como una lombriz!!")
