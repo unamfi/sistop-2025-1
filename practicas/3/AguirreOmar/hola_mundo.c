2eff5f569567a9627718a47f4c80e825b0ddf33d
omargabagu <omargabagu@gmail.com>
2024-08-27 23:48:02 -0600

    Modificar hola_mundo.c para saludar al usuario por nombre

--- practicas/3/AguirreOmar/hola_mundo.c
+++ practicas/3/AguirreOmar/hola_mundo.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hola Mundo\\n");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Hola, Mundo!\\n");
+    } else {
+        printf("Hola, %s!\\n", argv[1]);
+    }
     return 0;
 }
