0d1f162214047f2cf3f206e817adc43e434e6a9e
brend103 <floresr.brendal.p8@gmail.com>
2024-08-27 18:38:57 -0600

    Añade la variable de nombre

--- practicas/3/saludo.c
+++ practicas/3/saludo.c
@@ -1,6 +1,10 @@
 #include <stdio.h> 
 int main (){
-   printf("Hola mundo");
+	char nombre[20];
+	printf("Cual es tu nombre?");
+	scanf("%s", nombre);
+
+   printf("Hola mundo de %s", nombre);
    return 0;
 }
 
