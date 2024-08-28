f56c464eb44dab14d2ab1c975322ed3d4ab745b8
GabsLozada <gabyaquino203@gmail.com>
2024-08-27 18:37:50 -0600

    Modificación de HolaMundo.c

--- practicas/3/AquinoGabriela/HolaMundo.c
+++ practicas/3/AquinoGabriela/HolaMundo.c
@@ -2,8 +2,12 @@
 
 int main()
 {
+    char nombre[50];  
 
-    printf("Hola mundo :)");
-    
+    printf("Hola mundo :)");    
+    printf("¿Cuál es tu nombre? ");
+    scanf("%49s", nombre);  
+    printf("Hola %s, ¡bienvenido a mi repositorio! :)\n", nombre);  
+
     return 0;
 }
