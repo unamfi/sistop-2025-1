76b677725abf93ecb45cc0b17f472a325cc2bd6b
SebasEGR <sebasgayosso652@gmail.com>
2024-08-28 16:01:01 -0600

    Hola Mundo Modificado

--- practicas/3/GayossoSebastian/saludo.c
+++ practicas/3/GayossoSebastian/saludo.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World");
+int main() {
+    char nombre[50];
+
+    printf("Hello, World!\n");
+    printf("Introduce tu nombre: ");
+    fgets(nombre, sizeof(nombre), stdin);
+
+    printf("Hola mundo y hola, %s", nombre);
 
     return 0;
 }
