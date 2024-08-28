06078771144f15653adace78883bdb2051212c9d
SantiagoPali <doggiegames16@gmail.com>
2024-08-27 20:57:18 -0600

    Versión 2, ingresar nombre de usuario

--- practicas/3/PaliSantiago/HolaMundo1.py
+++ practicas/3/PaliSantiago/HolaMundo1.py
@@ -1,1 +1,6 @@
-print("Hola Mundo")
+print("--------------------------------------- ")
+userName = input("Ingrese su nombre de usuario: ")
+print("--------------------------------------- ")
+print("Hola {A}".format(A = userName))
+
+
