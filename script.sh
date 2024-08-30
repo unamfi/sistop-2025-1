0dd1712af0ef083ff928e40a0fd222eb5cadfbcc
EstebanArellanesConde <est3banconde@gmail.com>
2024-08-29 20:42:36 -0600

    Creé un script en bash que imprime un Hola, Mundo!

--- script.sh
+++ script.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-#SHEBANG y Hola, mundo!
-
-read -p "Hola Mundo!, Escribe tu nombre porfavor: " name
-echo -n "Hola, ¿Tú eres $name ? [y/n]: "
-read -r ans
-
-if [[ "$ans" = "y" ]]; then
-	echo "Un placer conocernos $name"
-else
-	echo "Una disculpa, me confundí de persona :("
-fi
+echo Hola, Mundo!
