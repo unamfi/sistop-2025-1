5d0f1f951b7ebafc7698cd805525a924ea3db37b
EstebanArellanesConde <est3banconde@gmail.com>
2024-08-29 20:27:32 -0600

    Modifiqué el programa para saludar al usuario por su nombre

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
-echo Hola, mundo!
+#!/bin/bash
+#SHEBANG y Hola, mundo!
+
+read -p "Hola Mundo!, Escribe tu nombre porfavor: " name
+echo -n "Hola, ¿Tú eres $name ? [y/n]: "
+read -r ans
+
+if [[ "$ans" = "y" ]]; then
+	echo "Un placer conocernos $name"
+else
+	echo "Una disculpa, me confundí de persona :("
+fi
