1e0c2d6662f70bf91eacfb42b123f37f449ff484
DocttUNAM <danielhgt4@gmail.com>
2024-09-15 21:46:27 -0600

    Añadi el programa que autogenera archivos

---
+++ practicas/4/HernandezDaniel/ProyectoPOO/src/Cuenta/.transparent-text-area {.py
@@ -0,0 +1,8 @@
+.transparent-text-area {
+    -fx-background-color: transparent; /* Fondo transparente */
+    -fx-control-inner-background: transparent; /* Fondo del área de contenido transparente */
+    -fx-opacity: 1.0; /* Opacidad del texto al 100% */
+}
+
+
+
