09468116d6d31026f8b25ebc83f18e9ca34bd399
Santiago Medina <sntgmedinaguzman@gmail.com>
2024-09-17 22:03:42 -0600

    Agrego salto de linea

--- practicas/4/MedinaSantiago/Hola.java
+++ practicas/4/MedinaSantiago/Hola.java
@@ -1,5 +1,5 @@
 public class Hola {
 	public static void main(String args[]) {
-		System.out.print("Hola Mundo y a todos los integrantes de La Champiza");
+		System.out.println("Hola Mundo y a todos los integrantes de La Champiza");
 	} 
 }
