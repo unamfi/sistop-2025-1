220237ba6efa51c4b1218f5fac7f1c8a51efe491
GabsLozada <gabyaquino203@gmail.com>
2024-09-17 18:51:04 -0600

    Hola Mundo en C

--- practicas/4/AquinoGabriela/Hola.c
+++ practicas/4/AquinoGabriela/Hola.c
@@ -1,16 +1,8 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
 {
-    printf("Holaaa, soy Aquino Gsbriela\n");
+    printf("Holaaa, soy Aquino Gabriela\n");
     printf("Bienvenido a mi practica 4 de Sistemas Operativos :)");
     
     return 0;
