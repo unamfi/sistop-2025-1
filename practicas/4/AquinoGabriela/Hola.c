6d45ff256c5967dae07542ee1076d1ae94602b58
GabsLozada <gabyaquino203@gmail.com>
2024-09-17 19:14:49 -0600

    Agregamos el fin de línea faltante

--- practicas/4/AquinoGabriela/Hola.c
+++ practicas/4/AquinoGabriela/Hola.c
@@ -2,8 +2,23 @@
 
 int main()
 {
+	int n1, n2, producto, suma;
+	
     printf("Holaaa, soy Aquino Gabriela\n");
     printf("Bienvenido a mi practica 4 de Sistemas Operativos :)");
     
+    printf("\n Introduzca un primer numero: ");
+    scanf("%d",&n1);
+    printf("\n Introduzca un segundo numero: ");
+    scanf("%d",&n2);
+    
+    suma= n1+n2;
+    producto= n1*n2;
+    
+    printf( "\n   La suma es: %d", suma );
+    printf( "\n\n   La multiplicaci%cn es: %d", 162, producto );
+
+    getch(); /* Pausa */
+
     return 0;
 }
