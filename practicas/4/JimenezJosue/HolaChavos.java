1c5859b45f922f6463621dee0ebb7f8fba9920da
JosJim275 <josjimingenieria@gmail.com>
2024-09-17 22:20:42 -0600

    HolaChavos con salto de linea

--- practicas/4/JimenezJosue/HolaChavos.java
+++ practicas/4/JimenezJosue/HolaChavos.java
@@ -1,6 +1,6 @@
 public class HolaChavos {
     public static void main(String[] args) {
-        System.out.print("Buenas tardes jovenes");
+        System.out.println("Buenas tardes jovenes");
     }
 
 }
