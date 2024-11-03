06d29fb5bfd20a314a943b295edf7eae063328a0
Tenorio Martinez Jesus Alejandro <tenoriomartinezjesusalejandro@gmail.com>
2024-11-03 14:03:35 -0600

    Agrego la implementacion para montar el disco en el sistema de archivos

--- proyectos/micro-sist-de-arch-multihilos/TenorioJesus/proyecto.py
+++ proyectos/micro-sist-de-arch-multihilos/TenorioJesus/proyecto.py
@@ -21,8 +21,55 @@ import struct
 import threading
 
 
-class SisArch():
+#definicion para listar los archivos
+def montaje(fiunamfs):
+    #se abre el fiunamfs.img en modo binario
+    with open(fiunamfs,'rb') as disco:
+        #se verifica el niombre del sistema
+        disco.seek(0)
+        if disco.read(8).decode('ascii')!='FiUnamFS':
+            print(f"\n Este programa esta diseñado solo para fiunamfs.img")
+        else:
+            print("\n Sistema: FiunamFS")
+        
+        #ahora vamos a validar la version!
+        disco.seek(10)
+        version = disco.read(4).decode('ascii')
+        if version != '25-1':
+            print("\n Al parecer este archivo no es del semestre 25-1")
 
-    def __init__(self):
-        pass 
+        else: print("\n Version: 25-1")
+
+        #especificamos etiqueta del volumen
+        disco.seek(20)
+        vol = struct.unpack('<I', disco.read(struct.calcsize('<I')))[0]
+        print(f"\n etiqueta volumen: {vol}")
+
+        #especificamos el tamaño del cluster :D
+        disco.seek(40)
+        size = struct.unpack('<I', disco.read(struct.calcsize('<I')))[0]
+        print(f"\n Tamño cluster: {size}")
+        
+        #especificamos el numero de clusteres
+        disco.seek(45)
+        numclusters = struct.unpack('<I', disco.read(struct.calcsize('<I')))[0]
+        print(f"\n Numero de clusteres: {size}")
+
+        #especificamos el numero de cluster de la unidad completa
+        disco.seek(40)
+        Numunit = struct.unpack('<I', disco.read(struct.calcsize('<I')))[0]
+        print(f"\n numero de cluster de la unidad completa: {Numunit}")
+
+
+def main():
+
+    #montamos el archivo
+    montaje("fiunamfs.img")
+
+    while True:
+        #simulamos la terminal de windows
+        entrda = input(f"{os.getcwd()}\\FiunamFS> ")
+
+
+main()
 
