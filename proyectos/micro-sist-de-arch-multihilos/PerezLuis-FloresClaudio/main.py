655617af097953b4095d94c67d1eb459d6ab5e6d
Claudio Flores García <112033179+B3GGS@users.noreply.github.com>
2024-11-04 13:05:37 -0600

    Add files via upload
    Eliminación de biblioteca 'simple_term_menu' e implementación de un menu por consola manual.

--- proyectos/micro-sist-de-arch-multihilos/PerezLuis-FloresClaudio/main.py
+++ proyectos/micro-sist-de-arch-multihilos/PerezLuis-FloresClaudio/main.py
@@ -1,44 +1,25 @@
 import os,struct,math
-from simple_term_menu import TerminalMenu
 from prettytable import PrettyTable
 from datetime import datetime
 
-
 class FSsistop():
+    # [El resto de la clase FSsistop permanece igual]
     def __init__(self,img_path):
-
         self.img_path=img_path
         self.superblock=self.read_superblock(img_path)
-
-        #print(self.superblock[40:44])
-        #print("tamaño de cluster:",struct.unpack('<i',self.superblock[40:44])[0])
         self.cluster_size=struct.unpack('<i',self.superblock[40:44])[0]
-
-        #print(self.superblock[45:49])
         print("tamaño del directorio en clusters:",struct.unpack('<i',self.superblock[45:49])[0])
         self.directory_size=struct.unpack('<i',self.superblock[45:49])[0]
-
-        #print(self.superblock[50:54])
-        #print("numero de clusters en la unidad:",struct.unpack('<i',self.superblock[50:54])[0])
         self.size=struct.unpack('<i',self.superblock[50:54])[0]
-
-
         self.directory=self.read_directory(img_path)
 
+    # [Mantener todos los métodos existentes de la clase FSsistop sin cambios]
     def read_superblock(self,path):
         with open(path, 'rb+') as f:
             data = f.read(64)
             id_fs, version = struct.unpack('<9s5s', data[:14])
-            #print(data)
-
-            #print(id_fs)
             id_fs=id_fs[0:-1]
-            #print(id_fs)
-
-            #print(version)
             version=version[1:]
-            #print(version)
-            
             if id_fs.decode('ascii') != 'FiUnamFS' or version.decode('ascii') != '25-1':
                 raise ValueError("El archivo no es un sistema de archivos FiUnamFS o la versión es incorrecta.")
             else:
@@ -47,9 +28,7 @@ class FSsistop():
     def read_directory(self,path):
         with open(path, 'rb+') as f:
             f.seek(self.cluster_size)
-            #print(self.cluster_size)
             data=[]
-            #prueba lectura del directorio
             for i in range(64):
                 temp = f.read(64)
                 data.append({
@@ -60,15 +39,6 @@ class FSsistop():
                     'created':temp[24:38].decode('ascii'),
                     'modified':temp[38:52].decode('ascii')
                 })
-                #print(data[i])
-
-            # data = f.read(64)
-            # print("Estado: ",data[0])
-            # print('nombre:',data[1:16].decode('ascii'))
-            # print('tamaño en bytes:',struct.unpack('<i',data[16:20])[0])
-            # print('cluster inicial:',struct.unpack('<i',data[20:24])[0])
-            # print('Hora y fecha de creacion:',data[24:38].decode('ascii'))
-            # print('Hora y fecha de modificacion:',data[38:52].decode('ascii'))
             return data
     
     def copyfromFS(self,dest_path,filename):
@@ -84,15 +54,11 @@ class FSsistop():
                 f.seek(0)
                 f.write(data)
 
-
     def copytoFS(self,src_path,filename):
         index=None
         used_clusters=[]
         for content in self.directory:
             if content['state'] == 46:
-                # for i in range(content['start_cluster'], 
-                #             math.ceil(content['start_cluster']+content['size']/self.cluster_size)):
-                #     used_clusters.append(i)
                 if content['name'] == filename:
                     print(f'Ya existe un archivo \'{filename}\' en el sistema de archivos')
                     return
@@ -102,64 +68,37 @@ class FSsistop():
                 
             elif not index:
                 index=self.directory.index(content)
-                #print(index)
 
         used_clusters=sorted(used_clusters,key=lambda tup: tup[1])     
-        #print(used_clusters)
 
         if not index:
             print('El directorio está lleno')
             return
-            #self.directory[i]
-            #print(self.directory[i])
         else:
             size=None
             create=None
             mod=None
             try:
                 stat = os.stat(src_path)
-
                 local_time = datetime.fromtimestamp(stat.st_ctime)
-                #print(local_time.strftime("%Y%m%d%H%M%S"))
                 create=local_time.strftime("%Y%m%d%H%M%S")
-
                 local_time = datetime.fromtimestamp(stat.st_mtime)
-                #print(local_time.strftime("%Y%m%d%H%M%S"))
                 mod=local_time.strftime("%Y%m%d%H%M%S")
-
                 size=stat.st_size
-                # print(os.stat(src_path).st_size)
-                # size=os.stat(src_path).st_size
-                # print(os.stat(src_path).st_ctime)
-                # create=os.stat(src_path).st_ctime
-                # create=datetime.utcfromtimestamp(create).strftime('%Y%m%d%H%M%S')
-                # print(create)
-                # print(os.stat(src_path).st_mtime)
-                # mod=os.stat(src_path).st_mtime
-                
             except FileNotFoundError:
                 print('FileNotFoundError: El archivo especificado no existe')
                 return
-            # req_clusters=math.ceil(size/self.cluster_size)
-            # print('req clusters',req_clusters)
             start_cluster=self.allocate(used_clusters,size)
-            #print('start cluster:',start_cluster)
             if not start_cluster:
                 print('No hay espacio en el sistema para este archivo')
             else:
                 self.write(src_path,filename,size,start_cluster,index,create,mod)
 
-
     def allocate(self,used_clusters,size):
         req_clusters=math.ceil(size/self.cluster_size)
-        #print(req_clusters)
         start_cluster = 5
-
-        # if (used_clusters[1][0]-start_cluster<req_clusters):
-        #     return start_cluster
         for entry in used_clusters:
             if entry[0]-start_cluster>req_clusters:
-                #print(entry[0]-start_cluster)
                 return start_cluster
             else:
                 start_cluster=entry[1]
@@ -172,15 +111,7 @@ class FSsistop():
         with open(src_path,'rb') as f:
             f.seek(0)
             file_data=f.read(src_size)
-        #     #print(file_data )
-        #print(type(filename))
         
-   
-        # filename,
-        # struct.pack('<i',src_size),
-        # struct.pack('<i',start_cluster),
-        # create.encode('ascii'),
-        # mod.encode('ascii'),
         binary_metadata=bytearray(b'.')
         binary_metadata+=(filename.ljust(14).encode('ascii')+b'\x00'+
                       struct.pack('<i',src_size)+
@@ -190,15 +121,10 @@ class FSsistop():
                       bytes(12)
         )   
         with open(self.img_path,'r+b') as fs:
-            #modificacion del directorio
             fs.seek(self.cluster_size+64*dir_idx)
             fs.write(binary_metadata)
-
-            #modificacion del espacio de datos
             fs.seek(self.cluster_size*start_cluster)
             fs.write(file_data)
-
-            #modificacion del directorio en memoria
             self.directory[dir_idx]={
                 'state':46,
                 'name':filename,
@@ -207,18 +133,11 @@ class FSsistop():
                 'created':create,
                 'modified':mod
             }
-            # for i in self.directory:
-            #     print(i)
-
 
     def delete(self,filename):
         for entry in self.directory:
-            size = None
-            start_cluster = None
             if entry['name']==filename:
                 index=self.directory.index(entry)
-                size = entry['size']
-                start_cluster = entry['start_cluster']
                 self.directory[index]={
                     'state':35,
                     'name':'--------------',
@@ -227,8 +146,6 @@ class FSsistop():
                     'created':'00000000000000',
                     'modified':'00000000000000'
                 }
-                # for i in self.directory:
-                #     print(i)
                 binary_metadata=bytearray(b'#')
                 binary_metadata+=(b'--------------'+b'\x00'+
                       bytes(8)+
@@ -241,9 +158,7 @@ class FSsistop():
                     f.write(binary_metadata)
                 return
 
-
         print(f'El archivo \'{filename}\' no se encuentra en el sistema de archivos')
-
 
     def find(self,filename) -> dict:
         for content in self.directory:
@@ -251,12 +166,10 @@ class FSsistop():
                 return content
         return None
 
-
     def list_dir(self):
         table = PrettyTable()
         table.field_names = ["Nombre", "Tamaño", "Creado", "Modificado"]
         for content in self.directory:
-            
             if content['state'] == 46:
                 c=content['created']
                 m=content['modified']
@@ -264,44 +177,42 @@ class FSsistop():
                                content['size'],
                                f'{c[0:4]}-{c[4:6]}-{c[6:8]}  {c[8:10]}:{c[10:12]}:{c[12:14]}', 
                                f'{m[0:4]}-{m[4:6]}-{m[6:8]}  {m[8:10]}:{m[10:12]}:{m[12:14]}'])
-        #table.reversesort=True
         print(table.get_string(sortby="Nombre"))
 
+def mostrar_menu():
+    print("\nOpciones FIunamFS:")
+    print("1. Listar contenido")
+    print("2. Copiar archivo de FIunamFS")
+    print("3. Agregar archivo a FIunamFS")
+    print("4. Eliminar archivo")
+    print("5. Salir")
+    return input("Seleccione una opción (1-5): ")
+
 def main():
-    
-    options = ["Listar contenido", "Copiar archivo de FIunamFS", "Agregar archivo a FIunamFS", "Eliminar archivo", "Salir"]
-    terminal_menu = TerminalMenu(options)
-    fs=FSsistop('fi.img')
-    while(True):
-        print("Opciones FIunamFS:")
-        opt_idx = terminal_menu.show()
-        print('->'+options[opt_idx]+'\n')
-        match options[opt_idx]:
-            case "Listar contenido":
-                fs.list_dir()
-                pass
-            case "Copiar archivo de FIunamFS":
-                fs.copyfromFS('mensaje.jpg','mensaje.jpg')
-                pass
-            case "Agregar archivo a FIunamFS":
-                fs.copytoFS('test.png','test.png')
-                pass
-            case "Eliminar archivo":
-                fs.delete('test.png')
-                pass
-            case "Salir":
-                exit(0)
+    fs = FSsistop('fi.img')
+    while True:
+        opcion = mostrar_menu()
+        print()  # Línea en blanco para mejor presentación
         
+        match opcion:
+            case "1":
+                fs.list_dir()
+            case "2":
+                nombre = input("Nombre del archivo a copiar: ")
+                destino = input("Nombre del archivo de destino: ")
+                fs.copyfromFS(destino, nombre)
+            case "3":
+                origen = input("Ruta del archivo a agregar: ")
+                nombre = input("Nombre para el archivo en FIunamFS: ")
+                fs.copytoFS(origen, nombre)
+            case "4":
+                nombre = input("Nombre del archivo a eliminar: ")
+                fs.delete(nombre)
+            case "5":
+                print("¡Hasta luego!")
+                exit(0)
+            case _:
+                print("Opción no válida. Por favor, seleccione una opción del 1 al 5.")
 
 if __name__ == "__main__":
     main()
-
-testfs=FSsistop('fi.img')
-testfs.list_dir()
-testfs.copyfromFS('lol','lol')
-testfs.copytoFS('test.png','test.png')
-testfs.list_dir()
-testfs.delete('test.png')
-testfs.list_dir()
-testfs.delete('test.png')
-#print(os.stat('lol.png').st_size)
