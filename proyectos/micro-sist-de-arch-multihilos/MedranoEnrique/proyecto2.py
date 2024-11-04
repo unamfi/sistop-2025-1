1a079418d1e2d38c9558d688f1b8836e26b5819b
Enrique Medrano Solano <enriquemedranorabak@hotmail.com>
2024-11-04 11:47:30 -0600

    Se crea un archivo .py para la realización del código para el proyecto, con una pequeña descripción del mismo y datos en general.

---
+++ proyectos/micro-sist-de-arch-multihilos/MedranoEnrique/proyecto2.py
@@ -0,0 +1,12 @@
+"""
+Proyecto 2 - (Micro) Sistema de Archivos Multihilos
+Author: Medrano Solano Enrique
+Materia: Sistemas Operativos (sistop-2025-1)
+lenguaje: Python 3.12.7 64 bits
+Entorno: Windows 11
+Fecha de entrega: 04/11/2024
+
+Descripción de proyecto: Este programa implementa un sistema de archivos y su sadministración de procesos del mismo
+el cual permitirá mediante una interfaz gráfica (GUI) listar el contenido del directorio, copiar y eliminar archivos tanto
+de FiUnamFS como de manera local.
+"""
