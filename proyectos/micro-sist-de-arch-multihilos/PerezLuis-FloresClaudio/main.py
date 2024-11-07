5e5b5b63856a625b0829a51b6f1da4d755bf8af1
Claudio Flores García <112033179+B3GGS@users.noreply.github.com>
2024-11-06 18:00:23 -0600

    main.py final
    Cambios hechos en main_concurrencia.py, importados a main.py para su entrega.

--- proyectos/micro-sist-de-arch-multihilos/PerezLuis-FloresClaudio/main.py
+++ proyectos/micro-sist-de-arch-multihilos/PerezLuis-FloresClaudio/main.py
@@ -1,36 +1,56 @@
 import os,struct,math
 from prettytable import PrettyTable
 from datetime import datetime
+import threading
+import queue
 
 class FSsistop():
-    # [El resto de la clase FSsistop permanece igual]
+    '''En esta clase se definen los metodos para la interaccion con el sistema de archivos
+    fiunamfs especificado para este proyecto'''
+
     def __init__(self,img_path):
         self.img_path=img_path
+        #lectura del superbloque (bits 0-64)
         self.superblock=self.read_superblock(img_path)
         self.cluster_size=struct.unpack('<i',self.superblock[40:44])[0]
         print("tamaño del directorio en clusters:",struct.unpack('<i',self.superblock[45:49])[0])
         self.directory_size=struct.unpack('<i',self.superblock[45:49])[0]
         self.size=struct.unpack('<i',self.superblock[50:54])[0]
+
+        #lectura del directorio, almacena una lista de diccionarios con los contenidos
         self.directory=self.read_directory(img_path)
 
-    # [Mantener todos los métodos existentes de la clase FSsistop sin cambios]
+        #Estructura de cola protegida para su ejecucion en paralelo, permite multiples productores y consumidores
+        self.data_queue = queue.Queue(maxsize=10)
+        self.fs_lock = threading.Lock()
+
     def read_superblock(self,path):
+        '''Realiza la lectura del superbloque y verifica el string identificador y de versión, 
+        regresa un arreglo de 64 bits con el contenido del superbloque'''
+
         with open(path, 'rb+') as f:
             data = f.read(64)
             id_fs, version = struct.unpack('<9s5s', data[:14])
             id_fs=id_fs[0:-1]
             version=version[1:]
+            #lanza un error si no encuentra el string identificador o la versión no coincide
             if id_fs.decode('ascii') != 'FiUnamFS' or version.decode('ascii') != '25-1':
                 raise ValueError("El archivo no es un sistema de archivos FiUnamFS o la versión es incorrecta.")
             else:
                 return data
 
-    def read_directory(self,path):
+    def read_directory(self,path : str):
+        '''Realiza la lectura del directorio, leyendo desde el cluster 1 hasta el 4, 
+        regresa una lista de diccionarios con el contenido de cada entrada del directorio\n
+        \'path\' hace referencia a la ruta del archivo .img que contiene el FS'''
+
         with open(path, 'rb+') as f:
+            #Posiciona el cursor al inicio del cluster 1
             f.seek(self.cluster_size)
             data=[]
             for i in range(64):
                 temp = f.read(64)
+                #Registra cada entrada (64 bits) del directorio
                 data.append({
                     'state':temp[0],
                     'name':temp[1:15].decode('ascii').strip(),
@@ -41,22 +61,71 @@ class FSsistop():
                 })
             return data
     
-    def copyfromFS(self,dest_path,filename):
+    def copyfromFS(self,dest_path : str,filename : str):
+        '''Define las instrucciones para copiar un archivo desde el sistema de archivos hacia la
+        computadora donde se ejecuta el programa\n
+        \'dest_path\': hace referencia al archivo donde se guardaran los datos en el host\n
+        \'filename\' hace referencia al nombre del archivo en el FS'''
+
+        #Busca el nombre del archivo dentro del contenido del directorio y regresa el diccionario con sus datos
         entry=self.find(filename)
+        #Termina su ejecucion si no se encuentra el archivo
         if not entry:
             print(f"{filename} no se encuentra en el sistema de archivos")
-        else:
-            with open(self.img_path, 'rb') as f:
-                f.seek(entry['start_cluster']*self.cluster_size)
-                data=f.read(entry['size'])
-            
-            with open(dest_path,'wb')as f:
-                f.seek(0)
-                f.write(data)
+            return
+        
+        #Numero de bits leidos en cada operacion de lectura
+        chunk_size = self.cluster_size
+        total_size = entry['size']
 
-    def copytoFS(self,src_path,filename):
+        #Funcion interna para la lectura del FS
+        def reader_thread():
+            with open(self.img_path, 'rb') as f:
+                f.seek(entry['start_cluster'] * self.cluster_size)
+                bytes_read = 0
+                while bytes_read < total_size:
+                    #lee 1KB o los datos faltantes si es que son <1KB
+                    chunk = f.read(min(chunk_size, total_size - bytes_read))
+                    if not chunk:
+                        break
+                    #Ingresa datos a la cola compartida
+                    self.data_queue.put(chunk)
+                    bytes_read += len(chunk)
+            self.data_queue.put(None)
+
+        #Funcion interna para la escritura en el host
+        def writer_thread():
+            with open(dest_path, 'wb') as f:
+                while True:
+                    #Saca datos de la cola compartida
+                    chunk = self.data_queue.get()
+                    #Continua hasta leer un objeto None
+                    if chunk is None:
+                        break
+                    f.write(chunk)
+                    #Indica al productor que la tarea de procesamiento ha terminado
+                    self.data_queue.task_done()
+
+        #La lectura del FS y la escritura en el host se ejecutan concurrentemente
+        reader = threading.Thread(target=reader_thread)
+        writer = threading.Thread(target=writer_thread)
+        
+        reader.start()
+        writer.start()
+        
+        reader.join()
+        writer.join()
+
+    def copytoFS(self,src_path :str,filename : str):
+        '''Copia un archivo del host en el FS
+        \'src_path\' hace referencia a la ruta del archivo del host\n
+        \'filename\' hace referencia al nombre con el que se guardara en el FS'''
+
+        #Indice de la entrada del directorio que contiene la informacion del archivo
         index=None
+        #Lista del cluster inicial y final de cada archivo, usada para la asignacion de espacio
         used_clusters=[]
+        #Verifica que el nombre no se repita, encuentra la primer entrada vacia en el directorio guarda el indice
         for content in self.directory:
             if content['state'] == 46:
                 if content['name'] == filename:
@@ -66,78 +135,124 @@ class FSsistop():
                 used_clusters.append((content['start_cluster'],
                                     math.ceil(content['start_cluster']+content['size']/self.cluster_size)))
                 
-            elif not index:
+            elif content['state'] == 35 and index is None:
                 index=self.directory.index(content)
 
-        used_clusters=sorted(used_clusters,key=lambda tup: tup[1])     
+        #Organiza de menor a mayor en funcion del cluster inicial
+        used_clusters=sorted(used_clusters,key=lambda tup: tup[1])  
+        #print(used_clusters)   
 
-        if not index:
+        #Termina si no se encontraron entradas vacias en el directorio
+        if index is None:
             print('El directorio está lleno')
             return
-        else:
-            size=None
-            create=None
-            mod=None
-            try:
-                stat = os.stat(src_path)
-                local_time = datetime.fromtimestamp(stat.st_ctime)
-                create=local_time.strftime("%Y%m%d%H%M%S")
-                local_time = datetime.fromtimestamp(stat.st_mtime)
-                mod=local_time.strftime("%Y%m%d%H%M%S")
-                size=stat.st_size
-            except FileNotFoundError:
-                print('FileNotFoundError: El archivo especificado no existe')
-                return
-            start_cluster=self.allocate(used_clusters,size)
-            if not start_cluster:
-                print('No hay espacio en el sistema para este archivo')
-            else:
-                self.write(src_path,filename,size,start_cluster,index,create,mod)
 
-    def allocate(self,used_clusters,size):
+        try:
+            #obtiene los metadatos del archivo del host
+            stat = os.stat(src_path)
+            local_time = datetime.fromtimestamp(stat.st_ctime)
+            create=local_time.strftime("%Y%m%d%H%M%S")
+            local_time = datetime.fromtimestamp(stat.st_mtime)
+            mod=local_time.strftime("%Y%m%d%H%M%S")
+            size=stat.st_size
+        except FileNotFoundError:
+            print('FileNotFoundError: El archivo especificado no existe')
+            return
+
+        #Obtiene el primer cluster inicial en el cual se puede almacenar el archivo
+        start_cluster = self.allocate(used_clusters,size)
+        if not start_cluster:
+            print('No hay espacio en el sistema para este archivo')
+            return
+        
+        #tamaño de cada lectura (1KB)
+        chunk_size = self.cluster_size
+
+        #Funcion interna para la lectura del host
+        def reader_thread():
+            with open(src_path, 'rb') as f:
+                bytes_read = 0
+                while bytes_read < size:
+                    #Guarda en la cola protegida
+                    chunk = f.read(min(chunk_size, size - bytes_read))
+                    if not chunk:
+                        break
+                    self.data_queue.put(chunk)
+                    bytes_read += len(chunk)
+            self.data_queue.put(None)
+
+        #Funcion interna para la escritura en el FS
+        def writer_thread():
+                #Guarda en un bytearray los datos que se escribiran en el directorio
+                binary_metadata=bytearray(b'.')
+                binary_metadata+=(filename.ljust(14).encode('ascii')+b'\x00'+
+                            struct.pack('<i',size)+
+                            struct.pack('<i',start_cluster)+
+                            create.encode('ascii')+
+                            mod.encode('ascii')+
+                            bytes(12))
+
+                with open(self.img_path,'r+b') as fs:
+                    #Guarda la informacion en el directorio
+                    fs.seek(self.cluster_size+64*index)
+                    fs.write(binary_metadata)
+                    
+                    #Guarda los datos del archivo
+                    fs.seek(self.cluster_size*start_cluster)
+                    while True:
+                        #Extrae de la cola protegida
+                        chunk = self.data_queue.get()
+                        if chunk is None:
+                            break
+                        fs.write(chunk)
+                        #Indica al productor que la tarea de procesamiento ha terminado
+                        self.data_queue.task_done()
+
+                #Guarda en memoria los datos del archivo
+                self.directory[index]={
+                    'state':46,
+                    'name':filename,
+                    'size':size,
+                    'start_cluster':start_cluster,
+                    'created':create,
+                    'modified':mod
+                }
+
+        #Ejecuta la lectura y escritura de manera concurrente
+        reader = threading.Thread(target=reader_thread)
+        writer = threading.Thread(target=writer_thread)
+        
+        reader.start()
+        writer.start()
+        
+        reader.join()
+        writer.join()
+
+    def allocate(self,used_clusters:list,size:int):
+        '''Determina el primer cluster del FS en el que se puede almacenar un archivo'''
         req_clusters=math.ceil(size/self.cluster_size)
+
+        #Inicio del espacio de datos
         start_cluster = 5
         for entry in used_clusters:
+            #Verifica si hay suficiente espacio entre dos entradas para el archivo
             if entry[0]-start_cluster>req_clusters:
                 return start_cluster
             else:
                 start_cluster=entry[1]
+        #Verifica si hay espacio al final de los archivos que se han almacenado
         if 1440-start_cluster>req_clusters:
             return start_cluster
         else:
             return None
 
-    def write(self,src_path,filename,src_size,start_cluster,dir_idx,create,mod):
-        with open(src_path,'rb') as f:
-            f.seek(0)
-            file_data=f.read(src_size)
-        
-        binary_metadata=bytearray(b'.')
-        binary_metadata+=(filename.ljust(14).encode('ascii')+b'\x00'+
-                      struct.pack('<i',src_size)+
-                      struct.pack('<i',start_cluster)+
-                      create.encode('ascii')+
-                      mod.encode('ascii')+
-                      bytes(12)
-        )   
-        with open(self.img_path,'r+b') as fs:
-            fs.seek(self.cluster_size+64*dir_idx)
-            fs.write(binary_metadata)
-            fs.seek(self.cluster_size*start_cluster)
-            fs.write(file_data)
-            self.directory[dir_idx]={
-                'state':46,
-                'name':filename,
-                'size':src_size,
-                'start_cluster':start_cluster,
-                'created':create,
-                'modified':mod
-            }
-
-    def delete(self,filename):
+    def delete(self,filename : str):
+        '''Elimina un archivo del FS\n
+        \'filename\' es el nombre del archivo a eliminar'''
         for entry in self.directory:
             if entry['name']==filename:
                 index=self.directory.index(entry)
+                #Borra la entrada del directorio en memoria
                 self.directory[index]={
                     'state':35,
                     'name':'--------------',
@@ -153,6 +268,7 @@ class FSsistop():
                       b'00000000000000'+
                       bytes(12)
                 )
+                #Se posiciona al inicio de la entrada del directorio y elimina la entrada, no se borran los datos del archivo
                 with open(self.img_path,'r+b') as f:
                     f.seek(self.cluster_size+index*64)
                     f.write(binary_metadata)
@@ -160,13 +276,15 @@ class FSsistop():
 
         print(f'El archivo \'{filename}\' no se encuentra en el sistema de archivos')
 
-    def find(self,filename) -> dict:
+    def find(self,filename : str) -> dict:
+        '''Determina si un archivo se encuentra en el FS t regresa el diccionario con sus datos'''
         for content in self.directory:
             if(content['name']==filename):
                 return content
         return None
 
     def list_dir(self):
+        '''Imprime una lista con todas las entradas del directorio'''
         table = PrettyTable()
         table.field_names = ["Nombre", "Tamaño", "Creado", "Modificado"]
         for content in self.directory:
@@ -188,6 +306,18 @@ def mostrar_menu():
     print("5. Salir")
     return input("Seleccione una opción (1-5): ")
 
+def test_string(s :str) -> bool:
+    '''Verifica que el nombre del archivo cumpla con la especificación'''
+    try:
+        if len(s) > 14:
+            print('El nombre del archivo debe ser una cadena ASCII <= 14 caracteres')
+            return False
+        s.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        print('El nombre del archivo debe ser una cadena ASCII <= 14 caracteres')
+        return False
+
 def main():
     fs = FSsistop('fi.img')
     while True:
@@ -199,11 +329,16 @@ def main():
                 fs.list_dir()
             case "2":
                 nombre = input("Nombre del archivo a copiar: ")
+                if not test_string(nombre):
+                    continue
                 destino = input("Nombre del archivo de destino: ")
                 fs.copyfromFS(destino, nombre)
             case "3":
+                print("Ingrese la ruta completa del archivo (ejemplo: /ruta/completa/archivo.txt)")
                 origen = input("Ruta del archivo a agregar: ")
                 nombre = input("Nombre para el archivo en FIunamFS: ")
+                if not test_string(nombre):
+                    continue
                 fs.copytoFS(origen, nombre)
             case "4":
                 nombre = input("Nombre del archivo a eliminar: ")
