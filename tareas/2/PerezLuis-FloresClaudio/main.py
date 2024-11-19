a1eb25ece30ed133ebc5509d696939e74a85af6a
Claudio Flores García <112033179+B3GGS@users.noreply.github.com>
2024-11-19 11:33:19 -0600

    RR, SPN, Colas múltiples
    Falta anexar cargas aleatorias, al menos 5 ejecuciones y FB, SRR

--- tareas/2/PerezLuis-FloresClaudio/main.py
+++ tareas/2/PerezLuis-FloresClaudio/main.py
@@ -1,74 +1,199 @@
-from typing import List
+from typing import List, Dict
 
 class Proceso:
-    def __init__(self,nombre: str,duracion: int,start_time: int)->None:
-        self.nombre=nombre
-        #t
-        self.duration=duracion
-        self.ticks=duracion
-        self.start=start_time
-        #Tiempo de respuesta T = tFinal - Tinicial
-        self.T=0
-        #Tiempo en espera E=T-t
-        self.E=0
-        #Proporción de penalización P=T/t
-        self.P=0
-        #Proporción de respuesta R=1/P
-        self.R=0
+    def __init__(self, nombre: str, duracion: int, start_time: int) -> None:
+        self.datos = [
+            start_time,   # tiempo_llegada
+            duracion,     # t (tiempo de ejecución)
+            duracion,     # tiempo_restante
+            0,            # inicio
+            0,            # fin
+            0,            # T (tiempo total)
+            0,            # E (tiempo de espera)
+            0             # P (proporción de penalización)
+        ]
+        self.nombre = nombre
 
 def reset_procesos():
-    datos=[
-        ['A',3,0],
-        ['B',5,1],
-        ['C',2,3],
-        ['D',5,9],
-        ['E',5,12]
+    datos = [
+        ['A', 3, 0],
+        ['B', 5, 1],
+        ['C', 2, 3],
+        ['D', 5, 9],
+        ['E', 5, 12]
     ]
-    procs=[]
-    for proc in datos:
-        print(proc)
-        procs.append(Proceso(proc[0],proc[1],proc[2]))
-    return procs
+    return {proc[0]: Proceso(proc[0], proc[1], proc[2]).datos for proc in datos}
 
-def FCFS(procesos:List[Proceso]):
-    print('FCFS:')
-    queue=[]
-    t_total=0
-    grafica=''
-    #calculo del tiempo
-    for proc in procesos:
-        t_total+=proc.ticks
+def tiempoTotal(carga):
+    return sum(proceso[1] for proceso in carga.values())
 
-    print('\tTiempo total: ',t_total)
+def FCFS(carga: Dict):
+    resultados = {"T": 0, "E": 0, "P": 0}
+    cola = []
+    grafica = ""
+    t_total = tiempoTotal(carga)
+    
+    for key, value in list(carga.items()):
+        if value[0] == 0:
+            cola.append(key)
+            carga[key][2] = carga[key][1]
+    
+    tik = 0
+    while tik < t_total:
+        if cola:
+            carga[cola[0]][2] -= 1
+            grafica += cola[0]
+            
+            if carga[cola[0]][2] == 0:
+                carga[cola[0]][4] = tik + 1
+                carga[cola[0]][5] = carga[cola[0]][4] - carga[cola[0]][0]
+                carga[cola[0]][6] = carga[cola[0]][5] - carga[cola[0]][1]
+                carga[cola[0]][7] = carga[cola[0]][5] / carga[cola[0]][1]
+                cola.pop(0)
+        else:
+            t_total += 1
+        
+        for key, value in list(carga.items()):
+            if value[0] == (tik + 1):
+                cola.append(key)
+                carga[key][2] = carga[key][1]
+        
+        tik += 1
+    
+    for tiempos in carga.values():
+        resultados["T"] += tiempos[5]
+        resultados["E"] += tiempos[6]
+        resultados["P"] += tiempos[7]
+    
+    resultados['T'] /= len(carga)
+    resultados['E'] /= len(carga)
+    resultados['P'] /= len(carga)
+    
+    print(f"FCFS: T = {resultados['T']:0.2f}, E = {resultados['E']:0.2f}, P = {resultados['P']:0.2f}")
+    print(grafica)
 
-    for i in range(t_total):
-        #Determina si hay un proceso listo
-        for proc in procesos:
-            if proc.start==i:
-                queue.append(proc)
+def RR(carga: Dict, quantum: int = 1):
+    resultados = {"T": 0, "E": 0, "P": 0}
+    cola = []
+    grafica = ""
+    t_total = tiempoTotal(carga)
+    conta_tik = 0
+    
+    for key, value in list(carga.items()):
+        if value[0] == 0:
+            cola.append(key)
+            carga[key][2] = carga[key][1]
+    
+    tik = 0
+    while tik < t_total:
+        if cola:
+            carga[cola[0]][2] -= 1
+            grafica += cola[0]
+            
+            if carga[cola[0]][2] == 0:
+                carga[cola[0]][4] = tik + 1
+                carga[cola[0]][5] = carga[cola[0]][4] - carga[cola[0]][0]
+                carga[cola[0]][6] = carga[cola[0]][5] - carga[cola[0]][1]
+                carga[cola[0]][7] = carga[cola[0]][5] / carga[cola[0]][1]
+                cola.pop(0)
+                conta_tik = 0
+            else:
+                conta_tik += 1
+        else:
+            t_total += 1
+        
+        for key, value in list(carga.items()):
+            if value[0] == (tik + 1):
+                cola.append(key)
+                carga[key][2] = carga[key][1]
+        
+        if conta_tik == quantum:
+            cola.append(cola.pop(0))
+            conta_tik = 0
+        
+        tik += 1
+    
+    for tiempos in carga.values():
+        resultados["T"] += tiempos[5]
+        resultados["E"] += tiempos[6]
+        resultados["P"] += tiempos[7]
+    
+    resultados['T'] /= len(carga)
+    resultados['E'] /= len(carga)
+    resultados['P'] /= len(carga)
+    
+    print(f"RR: T = {resultados['T']:0.2f}, E = {resultados['E']:0.2f}, P = {resultados['P']:0.2f}")
+    print(grafica)
 
-        #Hay un proceso en la cola
-        if queue:
-            queue[0].ticks-=1
-            grafica+=queue[0].nombre
-            if queue[0].ticks==0:
-                queue[0].T = i - queue[0].start + 1
-                queue[0].E = queue[0].T - queue[0].duration
-                queue[0].P = queue[0].T / queue[0].duration
-                queue[0].R = 1 / queue[0].P
-                print(f'\t{queue[0].nombre}: T={queue[0].T}, E={queue[0].E}, P={queue[0].P:.2f}, R={queue[0].R:.2f} ')
-                queue.pop(0)
+def SPN(carga: Dict):
+    resultados = {"T": 0, "E": 0, "P": 0}
+    cola = []
+    grafica = ""
+    t_total = tiempoTotal(carga)
+    
+    for key, value in list(carga.items()):
+        if value[0] == 0:
+            cola.append(key)
+            carga[key][2] = carga[key][1]
+    
+    if cola:
+        cola.sort(key=lambda x: carga[x][1])
+    
+    tik = 0
+    while tik < t_total:
+        if cola:
+            carga[cola[0]][2] -= 1
+            grafica += cola[0]
+            
+            if carga[cola[0]][2] == 0:
+                carga[cola[0]][4] = tik + 1
+                carga[cola[0]][5] = carga[cola[0]][4] - carga[cola[0]][0]
+                carga[cola[0]][6] = carga[cola[0]][5] - carga[cola[0]][1]
+                carga[cola[0]][7] = carga[cola[0]][5] / carga[cola[0]][1]
+                cola.pop(0)
+                
+                for key, value in list(carga.items()):
+                    if value[0] == (tik + 1):
+                        cola.append(key)
+                        carga[key][2] = carga[key][1]
+                
+                cola.sort(key=lambda x: carga[x][1])
+            else:
+                for key, value in list(carga.items()):
+                    if value[0] == (tik + 1):
+                        cola.append(key)
+                        carga[key][2] = carga[key][1]
+                
+                procesos_esperando = cola[1:]
+                procesos_esperando.sort(key=lambda x: carga[x][1])
+                cola[1:] = procesos_esperando
+        else:
+            t_total += 1
+        
+        tik += 1
+    
+    for tiempos in carga.values():
+        resultados["T"] += tiempos[5]
+        resultados["E"] += tiempos[6]
+        resultados["P"] += tiempos[7]
+    
+    resultados['T'] /= len(carga)
+    resultados['E'] /= len(carga)
+    resultados['P'] /= len(carga)
+    
+    print(f"SPN: T = {resultados['T']:0.2f}, E = {resultados['E']:0.2f}, P = {resultados['P']:0.2f}")
+    print(grafica)
 
-    print('\t'+grafica)
+def main():
+    procesos = reset_procesos()
+    print("Realizando FCFS: ")
+    FCFS(procesos.copy())
+    
+    print("\nRealizando RR: ")
+    RR(procesos.copy())
+    
+    print("\nRealizando SPN: ")
+    SPN(procesos.copy())
 
-
-
-
-
-    pass
-
-procesos=reset_procesos()
-print("----------------------------")
-FCFS(procesos)
-
-
+if __name__ == "__main__":
+    main()
