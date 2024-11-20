cb11744a7ddab803c739b60d8d43c900751adca3
lued2897 <lued2897@gmail.com>
2024-11-19 22:04:46 -0600

    pruebas colas multiples

--- tareas/2/PerezLuis-FloresClaudio/main.py
+++ tareas/2/PerezLuis-FloresClaudio/main.py
@@ -1,8 +1,9 @@
 import random
 from typing import List, Dict
+from copy import deepcopy
 
 class Proceso:
-    def __init__(self, nombre: str, duracion: int, start_time: int) -> None:
+    def __init__(self, nombre: str, duracion: int, start_time: int, prioridad: int) -> None:
         self.datos = [
             start_time,   # tiempo_llegada
             duracion,     # t (tiempo de ejecución)
@@ -11,7 +12,9 @@ class Proceso:
             0,            # fin
             0,            # T (tiempo total)
             0,            # E (tiempo de espera)
-            0             # P (proporción de penalización)
+            0,            # P (proporción de penalización)
+            prioridad,            # prioridad (para cola multiple)
+            3,            # numero de ejecuciones (para cola multiple)
         ]
         self.nombre = nombre
 
@@ -42,6 +45,7 @@ def FCFS(carga: Dict):
                 carga[cola[0]][7] = carga[cola[0]][5] / carga[cola[0]][1]  # Proporción de penalización (P).
                 cola.pop(0)  # Elimina el proceso de la cola.
         else:
+            grafica += "□"
             t_total += 1  # Aumenta el tiempo total si no hay procesos listos.
 
         # Agregar procesos que lleguen al instante actual.
@@ -165,6 +169,72 @@ def SPN(carga: Dict): # Similar a FCFS, pero organiza la cola según el tiempo r
     print(f"SPN: T = {resultados['T']:0.2f}, E = {resultados['E']:0.2f}, P = {resultados['P']:0.2f}")
     print(grafica)
 
+def FB(carga: Dict):
+    resultados = {"T": 0, "E": 0, "P": 0}
+    colas = [[],
+            [],
+            [],
+            [],
+            []
+            ]
+    grafica = ""
+    t_total = tiempoTotal(carga) + max(proceso[0] for proceso in carga.values())  # Considera los tiempos de llegada
+    
+    #print(list(carga.items()))
+    # for key, values in list(carga.items()):
+    #     print(key,'  ',values)
+    #     if values[0] == 0:
+    #         cola[values[8]].append(key)
+    #         #carga[key][2] = carga[key][1]
+    # print(cola)
+    
+    clock = 0
+    while clock<t_total:
+        #Carga los procesos conforme llegan
+        for key, values in list(carga.items()):
+            #print(key,'  ',values)
+            if values[0] == clock:
+                colas[values[8]].append(key)
+        print(colas)
+        #Recorre las colas de mayor a menor prioridad
+        for q in colas:
+            #Si hay elementos en la cola q
+            if q:
+                carga[q[0]][2]-=1
+                carga[q[0]][9]-=1 #Numero de veces que se ha ejecutado con esta prioridad
+
+                grafica+= q[0]
+                #print('mamada',carga[q[0]][2])
+                if (carga[q[0]][2]==0):
+                    #print('asdasd',q)
+                    carga[q[0]][4] = clock + 1  # Tiempo de fin
+                    carga[q[0]][5] = carga[q[0]][4] - carga[q[0]][0]  # Tiempo total
+                    carga[q[0]][6] = carga[q[0]][5] - carga[q[0]][1]  # Tiempo de espera
+                    carga[q[0]][7] = carga[q[0]][5] / carga[q[0]][1]  # Proporción de penalización
+                    q.pop(0)
+                elif carga[q[0]][9] == 0 and carga[q[0]][8]<4:
+                    #print('lkjlk',q)
+                    carga[q[0]][8]+=1 #Reduccion de prioridad
+                    carga[q[0]][9]=3
+                    colas[ carga[q[0]][8] ].append(q.pop(0))
+                    
+                
+                break
+        #print(clock,"/",t_total)
+        clock += 1
+    
+    # Calcular resultados promedio
+    for tiempos in carga.values():
+        resultados["T"] += tiempos[5]
+        resultados["E"] += tiempos[6]
+        resultados["P"] += tiempos[7]
+
+    resultados['T'] /= len(carga)
+    resultados['E'] /= len(carga)
+    resultados['P'] /= len(carga)
+
+    print(f"FB: T = {resultados['T']:0.2f}, E = {resultados['E']:0.2f}, P = {resultados['P']:0.2f}")
+    print(grafica)
 
 def generar_procesos_aleatorios(num_procesos: int = 5) -> Dict[str, List[int]]:
     """Genera una carga de procesos aleatorios"""
@@ -173,7 +243,8 @@ def generar_procesos_aleatorios(num_procesos: int = 5) -> Dict[str, List[int]]:
         nombre = chr(65 + i)  # Nombres A, B, C, etc.
         duracion = random.randint(1, 10)  # Duración aleatoria entre 1 y 10
         start_time = random.randint(0, 15)  # Tiempo de llegada aleatorio entre 0 y 15
-        procesos[nombre] = Proceso(nombre, duracion, start_time).datos
+        prioridad = random.randint(0,4)     #Prioridad del proceso para FB 0=max 4=min
+        procesos[nombre] = Proceso(nombre, duracion, start_time, prioridad).datos
     return procesos
 
 def main():
@@ -182,20 +253,24 @@ def main():
         print(f"\n--- Ejecución {ejecucion} ---\n")
         print("Datos generados: ")
         for nombre, datos in procesos.items():
-            print(f"{nombre}: Llegada={datos[0]}, Duración={datos[1]}")
+            print(f"{nombre}: Llegada={datos[0]}, Duración={datos[1]}, Prioridad={datos[8]}")
         # Calcular e imprimir el tiempo total
         t_total = tiempoTotal(procesos)
         print(f"\nTiempo total: {t_total}")
 
 
         print("\n")
-        FCFS(procesos.copy())
+        FCFS(deepcopy(procesos))
         
         print("\n")
-        RR(procesos.copy())
+        RR(deepcopy(procesos))
         
         print("\n")
-        SPN(procesos.copy())
+        SPN(deepcopy(procesos))
+        
+        #procesos = generar_procesos_aleatorios()
+        print("\n")
+        FB(deepcopy(procesos))
 
 if __name__ == "__main__":
     main()
